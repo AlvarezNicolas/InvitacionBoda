482c4134148c8a2974f23aef1d34acb1d8485d92
Nicolas Alvarez <anicolas92@hotmail.com>
2021-12-15 21:54:18 -0300

    Primer commit

---
+++ script/index.js
@@ -0,0 +1,101 @@
+function getTimeRemaining(endtime){
+    const total = Date.parse(endtime) - Date.parse(new Date());
+    const seconds = Math.floor( (total/1000) % 60 );
+    const minutes = Math.floor( (total/1000/60) % 60 );
+    const hours = Math.floor( (total/(1000*60*60)) % 24 );
+    const days = Math.floor( total/(1000*60*60*24) );
+    
+    return {
+      total,
+      days,
+      hours,
+      minutes,
+      seconds
+    };
+  }
+  
+  function initializeClock(id, endtime) {
+    const daysSpan = document.getElementById('days');
+    const hoursSpan = document.getElementById('hours');
+    const minutesSpan = document.getElementById('minutes');
+    const secondsSpan = document.getElementById('seconds');
+    
+    function updateClock(){
+      const t = getTimeRemaining(endtime);
+  
+      daysSpan.innerHTML = t.days;
+      hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+      minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+      secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+  
+      if (t.total <= 0) {
+        clearInterval(timeinterval);
+      }
+    }
+  
+    updateClock(); // run function once at first to avoid delay
+    var timeinterval = setInterval(updateClock,1000);
+  }
+  
+  const deadline = 'February 27 2022 11:45:00 GMT-0300';
+  initializeClock('clockdiv', deadline);
+  
+  $(".ace_btn").addcalevent({
+    'data': {
+      "title":"Anita & Tomás",
+      "desc":"Nos casamos ♥", 
+      "location": "Casa de Campo, Callejón Prof. Mathus, Bermejo, Guaymallen, Mendoza",
+      "url": "https://wonderful-jennings-88cc9e.netlify.app/",		
+      "time":{
+        "start":"February 27, 2022 11:45:00",
+        "end":"February 28, 2022 10:00:00",
+        "zone":"-03:00"
+      },
+    },
+    'vcalEnabled':true,
+    'ics': "../anitaytomás.ics"
+  });
+  
+  function copyToClipboard(string) {
+    let textarea;
+    let result;
+  
+    try {
+      textarea = document.createElement('textarea');
+      textarea.setAttribute('readonly', true);
+      textarea.setAttribute('contenteditable', true);
+      textarea.style.position = 'fixed'; // prevent scroll from jumping to the bottom when focus is set.
+      textarea.value = string;
+  
+      document.body.appendChild(textarea);
+  
+      textarea.focus();
+      textarea.select();
+  
+      const range = document.createRange();
+      range.selectNodeContents(textarea);
+  
+      const sel = window.getSelection();
+      sel.removeAllRanges();
+      sel.addRange(range);
+  
+      textarea.setSelectionRange(0, textarea.value.length);
+      result = document.execCommand('copy');
+    } catch (err) {
+      console.error(err);
+      result = null;
+    } finally {
+      document.body.removeChild(textarea);
+    }
+  
+    // manual copy fallback using prompt
+    if (!result) {
+      const isMac = navigator.platform.toUpperCase().indexOf('MAC') >= 0;
+      const copyHotkey = isMac ? '⌘C' : 'CTRL+C';
+      result = prompt(`Press ${copyHotkey}`, string); // eslint-disable-line no-alert
+      if (!result) {
+        return false;
+      }
+    }
+    return true;
+  }
